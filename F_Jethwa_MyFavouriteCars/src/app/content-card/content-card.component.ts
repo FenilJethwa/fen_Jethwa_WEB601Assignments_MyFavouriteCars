c1fb6c13683cd4a3c0aec9f29ce3d00394c62a2f
FenilJethwa <w0805286@myscc.ca>
2023-02-17 13:20:53 -0500

    update content card.component.html file with other file and added changes as professor asked

--- F_Jethwa_MyFavouriteCars/src/app/content-card/content-card.component.ts
+++ F_Jethwa_MyFavouriteCars/src/app/content-card/content-card.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Content } from '../content';
-
+import { Component, Input } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.css']
 })
+export class ContentCardComponent {
+  @Input() content: Content;
+  @Input() first: boolean;
 
-
+  displayDetails(): void {
+    console.log(`ID: ${this.content.id} - Title: ${this.content.title}`);
+  }
+}
--- F_Jethwa_MyFavouriteCars/src/app/content-list/content-list.component.ts
+++ F_Jethwa_MyFavouriteCars/src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.css']
 })
 export class ContentListComponent {
-
+  contents: Content[] = [
+    {
+      id: 1,
+      title: 'First Content',
+      description: 'This is the first content',
+      creator: 'John Doe',
+      imgURL: 'https://example.com/image1.jpg',
+      type: 'video',
+      tags: ['tag1', 'tag2']
+    },
+    {
+      id: 2,
+      title: 'Second Content',
+      description: 'This is the second content',
+      creator: 'Jane Doe',
+      imgURL: 'https://example.com/image2.jpg',
+      type: 'article',
+      tags: ['tag3', 'tag4']
+    },
+    {
+      id: 3,
+      title: 'Third Content',
+      description: 'This is the third content',
+      creator: 'Bob Smith',
+      imgURL: 'https://example.com/image3.jpg',
+      type: 'video',
+      tags: ['tag1', 'tag4']
+    },
+    {
+      id: 4,
+      title: 'Fourth Content',
+      description: 'This is the fourth content',
+      creator: 'Alice Smith',
+      imgURL: 'https://example.com/image4.jpg',
+      type: 'article',
+      tags: ['tag2', 'tag3']
+    },
+    {
+      id: 5,
+      title: 'Fifth Content',
+      description: 'This is the fifth content',
+      creator: 'Michael Johnson',
+      imgURL: 'https://example.com/image5.jpg',
+      type: 'video',
+      tags: ['tag1', 'tag3']
+    },
+    {
+      id: 6,
+      title: 'Sixth Content',
+      description: 'This is the sixth content',
+      creator: 'Emily Johnson',
+      imgURL: 'https://example.com/image6.jpg',
+      type: 'article',
+      tags: ['tag2', 'tag4']
+    },
+  ];
 }
