7b8aee8647213136f5e868979dd19c174b80873c
FenilJethwa <w0805286@myscc.ca>
2023-02-17 12:53:27 -0500

    added new file name content-list.ts and added method and attributes as professor asked

---
+++ F_Jethwa_MyFavouriteCars/src/app/helper-files/content-list.ts
@@ -0,0 +1,37 @@
+
+import { Content } from '../content-interface';
+
+
+export class ContentList {
+  private contentList: Content[];
+
+  constructor() {
+    this.contentList = [];
+  }
+
+  getContents(): Content[] {
+    return this.contentList;
+  }
+
+  addContent(content: Content): void {
+    this.contentList.push(content);
+  }
+
+  getContentCount(): number {
+    return this.contentList.length;
+  }
+
+  getContentHtml(index: number): string {
+    const content = this.contentList[index];
+    const imgTag = content.imgURL ? `<img src="${content.imgURL}" alt="${content.title}">` : '';
+    return `
+      <div>
+        <h2>${content.title}</h2>
+        ${imgTag}
+        <p>${content.description}</p>
+        <p>Creator: ${content.creator}</p>
+        <p>Type: ${content.type || 'N/A'}</p>
+      </div>
+    `;
+  }
+}
