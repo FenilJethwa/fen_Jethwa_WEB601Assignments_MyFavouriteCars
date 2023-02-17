e3b1f15c51c249d189960d608ce004c442b959c5
FenilJethwa <w0805286@myscc.ca>
2023-02-17 13:43:13 -0500

    added button and input as proffessor asked

--- F_Jethwa_MyFavouriteCars/src/app/pipes/ContentFilter.Pipe.ts
+++ F_Jethwa_MyFavouriteCars/src/app/pipes/ContentFilter.Pipe.ts
@@ -2,18 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 
 @Pipe({
-  name: 'contentFilter'
+  name: 'contentTypeFilter'
 })
-export class ContentFilterPipe implements PipeTransform {
-
-  transform(contents: Content[], typeFilter?: string): Content[] {
-    if (!contents) {
-      return [];
-    }
-    if (!typeFilter) {
+export class ContentTypeFilterPipe implements PipeTransform {
+  transform(contents: Content[], type?: string): Content[] {
+    if (!type) {
       return contents.filter(content => !content.type);
     }
-    return contents.filter(content => content.type === typeFilter);
+    return contents.filter(content => content.type === type);
   }
-
 }
