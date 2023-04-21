b0ce14f5dc448cf84c6ad6b7179510e6f7a85380
FenilJethwa <w0805286@myscc.ca>
2023-04-20 21:02:53 -0400

    Interface changes because of error

--- F_Jethwa_MyFavouriteCars/src/app/helper-files/content-interface.ts
+++ F_Jethwa_MyFavouriteCars/src/app/helper-files/content-interface.ts
@@ -1,9 +1,9 @@
-export interface Content {
+interface Content {
   id: number;
   title: string;
   description: string;
   creator: string;
   imgURL?: string;
   type?: string;
-  tags?: string[];
+  tags?: [string];
 }
